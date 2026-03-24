debc59df5e60521305969515f919ea9a06af7d42
sweetyparmar6116-ux <sweetyparmar6116@gmail.com>
2026-03-24 04:30:56 +0000

    update

--- week8_24_03_26/src/App.jsx
+++ week8_24_03_26/src/App.jsx
@@ -1,18 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Home from "./pages/Home";
 import BlogDetails from "./pages/BlogDetails";
 import AddBlog from "./pages/AddBlog";
 import Navbar from "./components/Navbar";
 
 function App() {
+
+  const [blogs, setBlogs] = useState([
+    { id: 1, title: "First Blog", content: "Hello World" },
+    { id: 2, title: "Second Blog", content: "React is easy" }
+  ]);
+
   return (
     <BrowserRouter>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home blogs={blogs} />} />
+        <Route path="/add" element={<AddBlog setBlogs={setBlogs} />} />
         <Route path="/blog/:id" element={<BlogDetails />} />
-        <Route path="/add" element={<AddBlog />} />
       </Routes>
     </BrowserRouter>
   );
