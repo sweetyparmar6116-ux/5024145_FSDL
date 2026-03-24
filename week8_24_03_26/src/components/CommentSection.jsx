4a890ce28c6344e118d209aa0b700d8d57d24c56
sweetyparmar6116-ux <sweetyparmar6116@gmail.com>
2026-03-24 04:24:40 +0000

    add

---
+++ week8_24_03_26/src/components/CommentSection.jsx
@@ -0,0 +1,33 @@
+import { useState, useRef } from "react";
+
+function CommentSection() {
+  const [comments, setComments] = useState([]);
+  const inputRef = useRef();
+
+  const addComment = () => {
+    const text = inputRef.current.value;
+    if (!text) return;
+
+    setComments([...comments, text]);
+
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+
+  return (
+    <div>
+      <h3>Comments</h3>
+
+      <input ref={inputRef} placeholder="Add comment" />
+      <button onClick={addComment}>Post</button>
+
+      <ul>
+        {comments.map((c, i) => (
+          <li key={i}>{c}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CommentSection;
