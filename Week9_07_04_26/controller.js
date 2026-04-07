aea289dd495d8d827a1dcdad9fe0c0a42c15e47f
sweetyparmar6116-ux <sweetyparmar6116@gmail.com>
2026-04-07 04:14:22 +0000

    commit

--- Week9_07_04_26/controller.js
+++ Week9_07_04_26/controller.js
@@ -1,11 +1,27 @@
 app.controller("myController", function($scope) {
 
+    $scope.appTitle = "AngularJS Hello World App";
+    $scope.appSubtitle = "A simple project using ng-controller, ng-model, and expressions.";
+
     $scope.name = "World";
+    $scope.counter = 0;
+    $scope.newItem = "";
 
     $scope.changeMessage = function() {
         $scope.name = "AngularJS";
     };
 
+    $scope.increment = function() {
+        $scope.counter += 1;
+    };
+
     $scope.items = ["Hello", "Welcome", "AngularJS"];
+
+    $scope.addItem = function() {
+        if ($scope.newItem) {
+            $scope.items.push($scope.newItem);
+            $scope.newItem = "";
+        }
+    };
 
 });
