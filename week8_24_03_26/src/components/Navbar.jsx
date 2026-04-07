2f2eefb5aa7b8e1be65e39edd7d81995996d42db
sweetyparmar6116-ux <sweetyparmar6116@gmail.com>
2026-04-07 03:29:10 +0000

    commit

--- week8_24_03_26/src/components/Navbar.jsx
+++ week8_24_03_26/src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import BlogItem from "./BlogItem";
+import { Link } from "react-router-dom";
 
-function BlogList({ blogs }) {
+function Navbar() {
   return (
-    <div>
-      {blogs.map((blog) => (
-        <BlogItem key={blog.id} blog={blog} />
-      ))}
-    </div>
+    <nav style={{
+      backgroundColor: "#333",
+      color: "white",
+      padding: "15px 20px",
+      marginBottom: "20px"
+    }}>
+      <div style={{
+        display: "flex",
+        gap: "20px",
+        justifyContent: "flex-start"
+      }}>
+        <Link to="/" style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
+          Home
+        </Link>
+        <Link to="/add" style={{ color: "white", textDecoration: "none", fontWeight: "bold" }}>
+          Add Blog
+        </Link>
+      </div>
+    </nav>
   );
 }
 
-export default BlogList;
+export default Navbar;
